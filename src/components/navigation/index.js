1765e754b6757cac1787af039cf1cb04fabb8f70
littlewing <mail@alexandra-jaeger.net>
2025-08-27 15:28:06 +0200

    Visual indication for current navigation link added

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -24,21 +24,28 @@ const Navigation = () => {
             </div>
             <ul className="nav-links">
                 <li key={"all"}>
-                    {/* These links should be NavLink component and add a special active class name if its an active link */}
-                    <NavLink to="/" className="nav-link">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `nav-link ${isActive ? "nav-link-active" : ""}`
+                        }
+                    >
                         All Pets
                     </NavLink>
                 </li>
                 {petTypes
                     ? petTypes.map((type) => (
                           <li key={type.name}>
-                              {/* These links should be NavLink component and add a special active class name if its an active link */}
                               <NavLink
                                   to={`/${type._links.self.href
                                       .split("/")
                                       .pop()}`}
                                   key={type.name}
-                                  className="nav-link"
+                                  className={({ isActive }) =>
+                                      `nav-link ${
+                                          isActive ? "nav-link-active" : ""
+                                      }`
+                                  }
                               >
                                   {type.name}s
                               </NavLink>{" "}
